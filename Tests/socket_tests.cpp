8210a8f1c71600fdbae2949b8907f7cdd14a0c61
dgercho <davidgercho1@gmail.com>
2023-03-11 18:56:29 +0200

    Fix test and format socket.cpp & socket.h

--- Tests/socket_tests.cpp
+++ Tests/socket_tests.cpp
@@ -1,20 +1,18 @@
 #include <gtest/gtest.h>
+
 #include "../src/socket.h"
 
 constexpr auto TEST_IP_ADDR = "127.0.0.1";
-constexpr auto TEST_PORT_1 = 9999;
-constexpr auto TEST_PORT_2 = 8888;
-constexpr auto TEST_PORT_3 = 5555;
+constexpr auto TEST_PORT = 9999;
 
-TEST(create_tcp_sock, BasicAssertions)
-{
+TEST(create_tcp_sock, BasicAssertions) {
   // Create a listening socket
   Socket sock1 = Socket(Socket::Protocol::TCP);
-  sock1.bind(TEST_PORT_1);
+  sock1.bind(TEST_PORT);
   sock1.listen(1);
   // Connect to listening socket
   Socket sock2 = Socket(Socket::Protocol::TCP);
-  sock2.connect(TEST_IP_ADDR, TEST_PORT_1);
+  sock2.connect(TEST_IP_ADDR, TEST_PORT);
   // Accept conneciton
   auto sock3 = sock1.accept();
   std::string demo_str = "hello";
@@ -28,51 +26,47 @@ TEST(create_tcp_sock, BasicAssertions)
   sock2.close();
   sock1.close();
   // Expect equality.
-  EXPECT_STREQ(demo_str.data(),recv_buffer.data());
+  EXPECT_STREQ(demo_str.data(), recv_buffer.data());
   EXPECT_EQ(recv_buffer.size(), demo_str.size());
 }
 
-TEST(try_bind_twice, HasCertainMessage)
-{
+TEST(try_bind_twice, HasCertainMessage) {
   // Bind a socket
   Socket sock1 = Socket(Socket::Protocol::TCP);
-  sock1.bind(TEST_PORT_2);
+  sock1.bind(TEST_PORT);
   // Bind socket with same port
   Socket sock2 = Socket(Socket::Protocol::TCP);
-  EXPECT_THROW({
-        try
-        {
-            sock2.bind(TEST_PORT_2);
+  EXPECT_THROW(
+      {
+        try {
+          sock2.bind(TEST_PORT);
+        } catch (const std::runtime_error& e) {
+          EXPECT_STREQ("Address already in use", e.what());
+          throw;
         }
-        catch( const std::runtime_error& e )
-        {
-            EXPECT_STREQ( "Address already in use", e.what() );
-            throw;
-        }
-    }, std::runtime_error);
+      },
+      std::runtime_error);
 
   sock2.close();
   sock1.close();
 }
 
-TEST(try_connect_to_closed_peer, BasicAssertions)
-{
+TEST(try_connect_to_closed_peer, BasicAssertions) {
   // Create a listening socket
   Socket sock1 = Socket(Socket::Protocol::TCP);
-  auto connect_result = sock1.connect(TEST_IP_ADDR,TEST_PORT_1);
+  auto connect_result = sock1.connect(TEST_IP_ADDR, TEST_PORT);
   // Expect connection error.
   EXPECT_EQ(connect_result, -1);
 }
 
-TEST(partial_recv, BasicAssertions)
-{
+TEST(partial_recv, BasicAssertions) {
   // Create a listening socket
   Socket sock1 = Socket(Socket::Protocol::TCP);
-  sock1.bind(TEST_PORT_3);
+  sock1.bind(TEST_PORT);
   sock1.listen(1);
   // Connect to listening socket
   Socket sock2 = Socket(Socket::Protocol::TCP);
-  sock2.connect(TEST_IP_ADDR, TEST_PORT_3);
+  sock2.connect(TEST_IP_ADDR, TEST_PORT);
   // Accept conneciton
   auto sock3 = sock1.accept();
   std::string demo_str = "abC1deFg%H";
@@ -82,12 +76,12 @@ TEST(partial_recv, BasicAssertions)
   std::vector<char> recv_buffer(demo_str.size());
   size_t total_read = 0;
   total_read += sock2.recv(recv_buffer.data(), 5);
-  total_read += sock2.recv(recv_buffer.data()+total_read, 5);
+  total_read += sock2.recv(recv_buffer.data() + total_read, 5);
   // Cleanup
   sock3.close();
   sock2.close();
   sock1.close();
   // Expect equality.
-  EXPECT_STREQ(demo_str.data(),recv_buffer.data());
+  EXPECT_STREQ(demo_str.data(), recv_buffer.data());
   EXPECT_EQ(recv_buffer.size(), demo_str.size());
 }
