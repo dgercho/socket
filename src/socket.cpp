0cd88dd3819da92ed1c63dcb44a9ba1d6412fedc
dgercho <dgercho@outlook.com>
2025-08-17 19:24:52 +0300

    Update socket class

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,103 +1,106 @@
 #include "socket.h"
 
 #include <cstring>
-#include <stdexcept>
-#include <system_error>
+#include "exceptions.h"
 
 constexpr auto SOCKET_ERROR_CODE = -1;
 
-Socket::Socket() :
-#ifdef _WIN32
-      m_socket(INVALID_SOCKET)
-#else
-      m_socket(0)
-#endif
+Socket::Socket()
 {
 #ifdef _WIN32
     if (WSAStartup(MAKEWORD(2, 2), &m_wsaData) != 0)
     {
-        throw std::runtime_error(std::strerror(errno));
+        throw SocketCreateException(std::strerror(errno));
     }
 #endif
-    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+    m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (m_socket == SOCKET_ERROR_CODE)
     {
 #ifdef _WIN32
         WSACleanup();
 #endif
-        throw std::runtime_error(std::strerror(errno));
+        throw SocketCreateException(std::strerror(errno));
     }
 }
 
-Socket::Socket(SOCKET socket) : m_socket(std::move(socket))
+Socket::Socket(SOCKET socket) : m_socket(socket) {}
+
+Socket::Socket(Socket&& other) noexcept : m_socket(other.m_socket) 
 {
+    other.m_socket = SOCKET_ERROR_CODE;
+
+}
+
+Socket& Socket::operator=(Socket&& other) noexcept {
+    m_socket = other.m_socket;
+    other.m_socket = SOCKET_ERROR_CODE;
 #ifdef _WIN32
-    if (WSAStartup(MAKEWORD(2, 2), &m_wsaData) != 0)
-    {
-        throw std::runtime_error(std::strerror(errno));
-    }
+    m_wsaData = other.m_wsaData;
+    ZeroMemory(&other.m_wsaData, sizeof(other.m_wsaData));
 #endif
+    return *this;
 }
 
-int Socket::connect(std::string address, int port)
+int Socket::Connect(std::string address, int port)
 {
-    sockaddr_in addr;
+    sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = inet_addr(address.c_str());
-    return ::connect(m_socket, (sockaddr *)&addr, sizeof(addr));
+    return ::connect(m_socket, reinterpret_cast<sockaddr *>(&addr), sizeof(addr));
 }
 
-void Socket::bind(int port)
+void Socket::Bind(int port)
 {
-    sockaddr_in addr;
+    sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (::bind(m_socket, (sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR_CODE)
+    if (::bind(m_socket, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == SOCKET_ERROR_CODE)
     {
-        throw std::runtime_error(std::strerror(errno));
+        throw SocketBindException(std::strerror(errno));
     }
 }
 
-void Socket::listen(int backlog)
+void Socket::Listen(int backlog)
 {
     if (::listen(m_socket, backlog) == SOCKET_ERROR_CODE)
     {
-        throw std::runtime_error(std::strerror(errno));
+        throw SocketListenException(std::strerror(errno));
     }
 }
 
-Socket Socket::accept()
+Socket Socket::Accept()
 {
     sockaddr_in client_addr;
     socklen_t client_addr_size = sizeof(client_addr);
     SOCKET client_socket = ::accept(m_socket, (sockaddr *)&client_addr, &client_addr_size);
     if (client_socket == SOCKET_ERROR_CODE)
     {
-        throw std::runtime_error(std::strerror(errno));
+        throw SocketAcceptException(std::strerror(errno));
     }
 
     return Socket(std::move(client_socket));
 }
 
-int Socket::send(const void *data, size_t length)
+int Socket::Send(const void *data, size_t length)
 {
     return ::send(m_socket, reinterpret_cast<const char *>(data), length, 0);
 }
 
-int Socket::recv(void *buffer, size_t length)
+int Socket::Receive(void *buffer, size_t length)
 {
     return ::recv(m_socket, reinterpret_cast<char *>(buffer), length, 0);
 }
 
-void Socket::close()
+void Socket::Close() noexcept
 {
     if (m_socket != SOCKET_ERROR_CODE)
     {
         int result = 0;
 #ifdef _WIN32
         result = ::closesocket(m_socket);
+        WSACleanup();
 #else
         result = ::close(m_socket);
 #endif
@@ -107,8 +110,5 @@ void Socket::close()
 
 Socket::~Socket()
 {
-    close();
-#ifdef _WIN32
-    WSACleanup();
-#endif
+    Close();
 }
