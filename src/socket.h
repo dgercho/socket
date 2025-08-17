b83cbfacdf9cdb79f84157c99324a86e756a89bf
David <davidgercho1@gmail.com>
2025-08-17 17:14:55 +0300

    Update functions

--- src/socket.h
+++ src/socket.h
@@ -1,8 +1,6 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
-#include "exceptions.h"
-
 #ifdef _WIN32
 #include <winsock2.h>
 #pragma comment(lib, "Ws2_32.lib")
@@ -18,48 +16,49 @@ typedef int SOCKET;
 
 class Socket {
  public:
-
-  /// @brief Default constructor of `Socket`.
   Socket();
-
-  ~Socket();
+  explicit Socket(SOCKET socket);
+  Socket(const Socket&) = delete;
+  Socket(Socket&& other) noexcept;
+  Socket& operator=(Socket&& other) noexcept;
+  Socket& operator=(const Socket&) = delete;
+  
+  // Destructor, Close the socket
+  virtual ~Socket();
 
   /// @brief Connect to endpoint.
   /// @param address (std::string).
   /// @param port (int).
   /// @return operation result (int) - `0` if connected, `-1` if there was an
   /// error.
-  int connect(std::string address, int port);
+  int Connect(std::string address, int port);
 
   /// @brief Bind operation.
   /// @param port - port to bind.
-  void bind(int port);
+  void Bind(int port);
 
   /// @brief Allows to listen for connections.
   /// @param backlog (int) - number of pending connections the socket will hold.
-  void listen(int backlog);
+  void Listen(int backlog);
 
   /// @brief Accept new connections.
   /// @return A new `Socket` class.
-  Socket accept();
+  Socket Accept();
 
   /// @brief Close the socket.
-  void close();
+  void Close();
 
   /// @brief Send data to endpoint.
   /// @param data (const void *) - pointer to data buffer.
   /// @param length (size_t) - length of buffer to send.
   /// @return (int) The number of bytes accepted by the kernel for sending.
-  int send(const void *data, size_t length);
+  int Send(const void *data, size_t length);
 
   /// @brief Receive data from endpoint.
   /// @param buffer (void *) - buffer to write received data.
   /// @param length (size_t) - length to read from socket.
   /// @return (int) The number of received bytes.
-  int recv(void *buffer, size_t length);
-
- protected:
-  Socket(SOCKET socket);
+  int Receive(void *buffer, size_t length);
 
  private:
   int m_socket;
